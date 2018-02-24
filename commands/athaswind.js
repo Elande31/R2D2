14aa4b43de1e2bb265127f766fe58c87d2594f61
Elande <lubos.dovicic@gmail.com>
2018-02-24 20:30:46 +0100

    Delete athaswind.js

--- commands/athaswind.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-  return message.channel.send("All hail the Queen!");
-}
-
-module.exports.cmdCore = {
-  name: "athaswind"
-}
