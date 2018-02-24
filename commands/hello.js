37728de98267f716551792ccdc9a0ff873d519ac
Elande <lubos.dovicic@gmail.com>
2018-02-24 20:32:28 +0100

    Delete hello.js

--- commands/hello.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-  return message.channel.send("Oh, hello there!");
-}
-
-module.exports.cmdCore = {
-  name: "hello"
-}
